431b537f39c79af3099d1e210f64690d03f6fe76
vikas <yadav4228@gmail.com>
2023-12-17 14:56:54 +0530

    vikas ka commit

---
+++ git.sh
@@ -0,0 +1,5 @@
+git remote remove origin
+git config --global user.name 'vikas'
+git config --global user.email 'yadav4228@gmail.com'
+git remote add origin https://github.com/bcs1151/testing_git.git
+
